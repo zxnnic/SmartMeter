8fc6669f0052ab383fc8f722b99115ba361f3eb1
Nicule <zzhao1@ualberta.ca>
2020-01-21 20:38:43 -0700

    General functionality of Database class done
    Most of the work on the Databse class is finished, some parts that still are under review only include the output queries so that we can access and graph them.

--- setup.py
+++ setup.py
@@ -44,18 +44,22 @@ class Database():
                                         database = self.__name)
             # create a cursor
             self.cursor = self.connection.cursor()
-            # check 1: print postgres connection properties
-            print ( self.connection.get_dsn_parameters(),"\n")
 
-            # Print PostgreSQL version
-            self.cursor.execute("SELECT version();")
-            record = self.cursor.fetchone()
+            # Check 1: print postgres connection properties
+            # uncomment the print to see details
+            # print (self.connection.get_dsn_parameters(),"\n")
+            # Check 2: PostgreSQL version
+            # self.cursor.execute("SELECT version();")
+            # record = self.cursor.fetchone()
+            # print(record)
+        except psycopg2.Error as err:
+            print("ERROR:" + err)
         except Exception as err:
             print("ERROR:")
             for arg in err.args:
                 print(arg)
         else:
-            print("You are connected to ", record,"\n")
+            print("You are connected to", self.connection.get_dsn_parameters()['dbname'])
             print('Now you can continue on :^)\n')
             return True
         
@@ -65,17 +69,63 @@ class Database():
             self.connection.close()
             print("PostgreSQL connection is closed.")
 
-    def create_table(self, table_name):
-    pass
+    def createTable(self, table_name, primary_key, opt_args=None):
+        query = 'CREATE TABLE ' + table_name + '(' + primary_key[0] +' '+ primary_key[1] + ' NOT NULL,'
+        if opt_args is not None:
+            for arg in opt_args:
+                query += arg[0] + ' ' + arg[1] + ','
+        query += 'PRIMARY KEY (' + primary_key[0] + '));'
 
-    def insert(self, table_name, id, value):
-        pass
+        if self.execute(query):
+            print(table_name + ' successfully deleted.')
+    
+    def deleteTable(self, table_name):
+        if self.execute('DROP TABLE ' + table_name +';'):
+            print(table_name + ' successfully deleted.')
 
-    def delete(self, table_name, id):
-        pass
+    def deleteRow(self, table_name, condition):
+        if self.execute('DELETE FROM',table_name,'WHERE',condition):
+            print('Rows containing',condition,'in',table_name,'has been deleted.')
 
-    def delete_table(self, table_name):
-        pass
+    def insert(self, table_name, columns, values):
+        # This method is still in question as of whether or not it should be kept
+        # I am not sure if we even need the insert function
+        query = 'INSERT INTO '+ table_name +' ('
+        for column in columns:
+            query += column +','
+        query += ') VALUES ('
+        for value in values:
+            query += value + ','
+        query += ');'
+        
+        if self.execute(query):
+            print('Items successfully inserted into',table_name)
+
+    def deleteColumn(self, table_name, column_name):
+        if self.execute('ALTER TABLE', table_name, 'DROP COLUMN', column_name):
+            print('Column', column_name, 'has been deleted.')
+    
+    ###############################################################################
+    '''THESE TWO FUNCTIONS STILL ARE UNDER REVIEW'''
+    def getColumn(self, column, table_name, condition=None):
+        query = 'SELECT ' + column + ' FROM ' +table_name
+        if condition is not None:
+            query += 'WHERE ' + 
+    
+    def getTable(self, table_name):
+        query = 'SELECT * FROM ' + table_name +';'
+    ''''''
+    ################################################################################
+
+    def execute(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except (Exception, psycopg2.DatabaseError) as err:
+            print(err)
+            return False
+        else:
+            return True
 
     def getHost(self):
         return self.__host
@@ -93,6 +143,7 @@ if __name__ == "__main__":
                         '127.0.0.1', 
                         '5432',
                         'smart_meter_db')
+    db.setup()
     # Test all the different functions offered by the database class
     # Perform the tasks that the user wants
     done = False
@@ -103,11 +154,19 @@ if __name__ == "__main__":
         print('\t3) Insert into a table')
         print('\t4) Drop an entry from a table')
         print('\t5) Exit')
+
         user_input = int(input('What would you like to do? \n> '))
         if user_input == 1:
-            db.create_table('hehe')
+            # Each attribute/column of the table will need two parameters:
+            #           (name, data type)
+            primary_key = ('time', 'TIME')
+            attributes = [  ('current','FLOAT'),
+                            ('voltage','FLOAT'),
+                            ('phasor','FLOAT'),
+                            ('power','FLOAT')]
+            db.createTable(table_name='energy_use', primary_key=primary_key, opt_args=attributes)
         elif user_input == 2:
-            db.delete_table('hehe')
+            db.deleteTable('energy_use')
         elif user_input == 3:
             db.insert('hehe',1,'hellow')
         elif user_input == 4:
