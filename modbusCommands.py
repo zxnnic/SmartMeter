dff5cd87902797a38b49244c7ec50a1c7d99488d
ewhitby <ewhitby@ualberta.ca>
2020-02-25 22:06:09 -0700

    Changes after testing

--- modbusCommands.py
+++ modbusCommands.py
@@ -1,79 +1,110 @@
 import pymodbus
+import asyncio
+
+
+from pymodbus.client.asynchronous.serial import (
+    AsyncModbusSerialClient as ModbusClient)
+from pymodbus.client.asynchronous import schedulers
 
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
 from pymodbus.server.asynchronous import StartSerialServer
 from pymodbus.client.sync import ModbusSerialClient
+from pymodbus.transaction import ModbusAsciiFramer
 
 import time
 
 # Configure the service logging
 import logging
-logging.basicConfig(filename='modbusData.log',level=logging.DEBUG)
+logging.basicConfig()
 log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 # Data Register addresses
 VOLTS_L1_TO_NEUTRAL = 44059
 AMPS_L1 = 44056
 KVA = 44013
 
-# Initialize PowerScout as remote datastore 
+# Initialize PowerScout as remote datastore
 # note: MIGHT REQUIRE ModbusSparseDataBlock instead!!
-powerScout = ModbusSlaveContext(
-    di = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # discrete input contacts
-    co = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # discrete output coils
-    hr = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # analog output holding registers
-    ir = ModbusSequentialDataBlock(0x0000, [16]*0x270E)) # analog input registers
-context = ModbusServerContext(slaves=powerScout, single=True)
-
-client = ModbusSerialClient(method='ascii',port='/dev/ttyTHS2',parity='N',stopbits=1,bytesize=8,baudrate=9600,timeout=3)
-# Establish connection
-client.connect()
+#print('setting up the power scout')
+#powerScout = ModbusSlaveContext(
+#    di = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # discrete input contacts
+#    co = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # discrete output coils
+#    hr = ModbusSequentialDataBlock(0x0000, [16]*0x270E), # analog output holding registers
+#    ir = ModbusSequentialDataBlock(0x0000, [16]*0x270E)) # analog input registers
+#context = ModbusServerContext(slaves=powerScout, single=True)
 
 # Run the server
-StartSerialServer(context, port='/dev/pts/3', timeout=1)
+print('starting serial server')
+#StartSerialServer(context, port='/dev/ttyTHS2', framer=ModbusAsciiFramer, timeout=1)
+print('done connecting to server!')
 
+async def start_async_test(client):
 # Send data request and log response
-rr = client.read_holding_registers(VOLTS_L1_TO_NEUTRAL,1,unit=0) # address, count # of registers to read, slave address
+try:
+print('we in')
+# address, count # of registers to read, slave address
+log.debug("Read register for volts")
+rr = await client.read_holding_registers(4055, 1, unit = 0x01)
+#rr = await client.read_holding_registers(4205, 5, unit=0x01)
+log.debug("After two reads")
+buffer = "Requested value %d\n" % rr
+log.info(buffer)
+print('first buffer down!')
+print(buffer)
+rr = await client.read_holding_registers(AMPS_L1,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
 log.info(buffer)
 
-rr = client.read_holding_registers(AMPS_L1,1,unit=0)
+rr = client.read_holding_registers(KVA,1,unit=0x01)
+buffer = "Requested value %d\n" % rr
+
+log.info(buffer)
+
+#time.sleep(20)
+
+rr = client.read_holding_registers(VOLTS_L1_TO_NEUTRAL,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
 log.info(buffer)
 
-rr = client.read_holding_registers(KVA,1,unit=0)
+rr = client.read_holding_registers(AMPS_L1,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
 log.info(buffer)
 
-time.sleep(20)
+rr = client.read_holding_registers(KVA,1,unit=0x01)
+buffer = "Requested value %d\n" % rr
 
-rr = client.read_holding_registers(VOLTS_L1_TO_NEUTRAL,1,unit=1) # test unit=1
+log.info(buffer)
+
+#time.sleep(20)
+
+rr = client.read_holding_registers(VOLTS_L1_TO_NEUTRAL,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
 log.info(buffer)
 
-rr = client.read_holding_registers(AMPS_L1,1,unit=0)
+rr = client.read_holding_registers(AMPS_L1,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
 log.info(buffer)
 
-rr = client.read_holding_registers(KVA,1,unit=0)
+rr = client.read_holding_registers(KVA,1,unit=0x01)
 buffer = "Requested value %d\n" % rr
+
 log.info(buffer)
 
-time.sleep(20)
-
-rr = client.read_holding_registers(VOLTS_L1_TO_NEUTRAL,1,unit=0)
-buffer = "Requested value %d\n" % rr
-log.info(buffer)
-
-rr = client.read_holding_registers(AMPS_L1,1,unit=0)
-buffer = "Requested value %d\n" % rr
-log.info(buffer)
-
-rr = client.read_holding_registers(KVA,1,unit=0)
-buffer = "Requested value %d\n" % rr
-log.info(buffer)
-
-time.sleep(20)
-
+#time.sleep(20)
+except Exception as e:
+for err in e.args:
+print(err)
+finally:
+print(rr)
 client.close()
+
+if __name__ == '__main__':
+   
+    loop, client = ModbusClient(schedulers.ASYNC_IO, port='/dev/ttyTHS2',
+                                baudrate=9600, method='ascii', parity='N', stopbits=1, bytesize=8)
+    client.connect()
+    print('finished clienting')
+    loop.run_until_complete(start_async_test(client.protocol))
+    loop.close()
